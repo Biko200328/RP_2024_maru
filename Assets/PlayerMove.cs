13e121aacfb47bd303adacbbd36d615fcb0e7d29
HirokiYamamoto <hiroki200328@icloud.com>
2024-05-14 18:20:55 +0900

    ジャンプ制限

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -8,16 +8,22 @@ public class PlayerMove : MonoBehaviour
 	float speed = 0.5f;
 	public float radius = 0.5f; //円の大きさ
 
+	public bool isFloor;
+	public bool isDoubleJump;
+	public float jumpPower;
+
 	float _x;
 	float _z;
 
 	public GameObject lookObj;
-	public float jumpPower;
+	
+
+	Rigidbody rb;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
@@ -36,11 +42,33 @@ public class PlayerMove : MonoBehaviour
 		_z = radius * Mathf.Cos(speed);
 
 		transform.position = new Vector3(_x, transform.position.y,_z);
-		transform.LookAt(lookObj.gameObject.transform);
 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
+			var v = rb.velocity;
 			
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				//地面についてる時はジャンプ可能
+				if(isFloor == true)
+				{
+					v.y = 0;
+					v.y += jumpPower;
+				}
+				//ついていないときでもダブルジャンプフラグが残ってる時はできる
+				else
+				{
+					if(isDoubleJump == true)
+					{
+						v.y = 0;
+						v.y += jumpPower;
+						isDoubleJump = false;
+					}
+				}
+				
+			}
+
+			rb.velocity = v;
 		}
 	}
 }
