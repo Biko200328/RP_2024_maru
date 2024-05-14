61f04d6981f3cee29626c383046e579a8b8a4dee
HirokiYamamoto <hiroki200328@icloud.com>
2024-05-14 15:26:54 +0900

    2daysJamスタート

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+	public float addSpeed = 0.1f;
+	float speed = 0.5f;  //ƒJƒƒ‰‚ÌˆÚ“®‘¬“x
+	public float radius = 0.5f; //‰~‚Ì‘å‚«‚³
+
+	float _x;
+	float _z;
+
+	public GameObject playerObj;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.A))
+		{
+			speed += -addSpeed;
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			speed += addSpeed;
+		}
+
+		_x = radius * Mathf.Sin(speed);
+		_z = radius * Mathf.Cos(speed);
+
+		transform.position = new Vector3(_x, transform.position.y, _z);
+		transform.LookAt(playerObj.gameObject.transform);
+	}
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float addSpeed = 0.1f;
+	float speed = 0.5f;  //ƒJƒƒ‰‚ÌˆÚ“®‘¬“x
+	public float radius = 0.5f; //‰~‚Ì‘å‚«‚³
+
+	float _x;
+	float _z;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetKey(KeyCode.A))
+		{
+			speed += -addSpeed;
+		}
+		else if (Input.GetKey(KeyCode.D))
+		{
+			speed += addSpeed;
+		}
+
+		_x = radius * Mathf.Sin(speed);
+		_z = radius * Mathf.Cos(speed);
+
+		transform.position = new Vector3(_x, 0, _z);
+	}
+}
